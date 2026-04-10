d0f019c908e42650c69a904fa34ab149138a8723
Sarsoura710 <sarahlass12345@gmail.com>
2026-04-10 01:40:41 +0100

    added a resseting password page

---
+++ frontend/src/pages/ResettingPassword.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import "./ResettingPassword.css"
+import Template from "./component/Template.jsx"
+
+const ResettingPassword =  () => {
+
+    return(
+        <div>
+            <Template title="Reset Password" button_text="Change Password">
+                <div className='input_grp'>
+                  <label htmlFor="Password">Password</label>
+                  <input type="password" id="Password" placeholder="Enter your password" />
+                </div>
+                <div className='input_grp'>
+                  <label htmlFor="ConfirmPassword">Confirm Password</label>
+                  <input type="password" id="ConfirmPassword" placeholder="Confirm your password" />
+                </div>
+            </Template>
+        </div>
+
+    );
+};
+
+export default ResettingPassword;
