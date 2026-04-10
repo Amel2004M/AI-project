0aebb5e589e2bac9c64ae1bb9d7b46c9713936db
Sarsoura710 <sarahlass12345@gmail.com>
2026-04-10 01:35:55 +0100

    added a Reset password page

---
+++ frontend/src/pages/ResetPassword.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import "./ResetPassword.css"
+import Template from "./component/Template.jsx"
+
+const ResetPassword =  () => {
+
+    return(
+        <div>
+            <Template title="Reset Password" button_text="Send Code">
+                <p>Enter your email below to receive your code</p>
+                <div className='input_grp'>
+                  <label htmlFor="E-mail">E-mail adress</label>
+                  <input type="email" id="E-mail" placeholder="Enter your e-mail" />
+                </div>
+            </Template>
+        </div>
+    );
+};
+
+export default ResetPassword;
